37cb77c49063f6dfac01b172721ec5edd3699992
dayoon <dayo0319@naver.com>
2022-05-27 04:52:40 +0900

    commit

--- src/test/java/com/stock/capstone/CapstoneApplicationTests.java
+++ src/test/java/com/stock/capstone/CapstoneApplicationTests.java
@@ -2,14 +2,16 @@ package com.stock.capstone;
 
 
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CapstoneApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 
 	}
 
